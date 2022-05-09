b3c90241f9ff3d3604156db0b71f541c01eeed56
wipp <wippstkk@gmail.com>
2022-05-09 23:59:32 +0700

    test merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import TestWipp from "./TestWipp";
 
 function App() {
   return (
     <div className="App">
       <h1>this is my opal code</h1>
+      <TestWipp />
     </div>
   );
 }
