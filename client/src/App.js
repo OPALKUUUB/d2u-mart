30260833ecc0a9697bcf6d52ada5d4d35cbf0bc5
OPALKUUUB <makara.atipat@gmail.com>
2022-05-10 00:01:27 +0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import TestOpal from "./TestOpal";
 
 function App() {
   return (
     <div className="App">
+      <TestOpal />
       <h1>This is wipp's code</h1>
     </div>
   );
