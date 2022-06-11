743fc0884fee99f924e5c165c270357b14f2cde8
whi7ent <wippstkk@gmail.com>
2022-06-11 20:55:41 +0700

    add subcard

--- client/src/website/page/Mart/MartShop.js
+++ client/src/website/page/Mart/MartShop.js
@@ -2,13 +2,18 @@ import React from "react";
 import Card from "../../../system/component/Card/Card";
 import CardExpand from "../../../system/component/Card/CardExpand";
 import CardHeader from "../../../system/component/Card/CardHeader";
+import SubCard from "../../../system/component/SubCard/SubCard";
+import "./MartMenu.css";
 
 
 const MartShop = () => {
     return (
       <section style={{margin: "200px"}}>
         <CardHeader text = "ร้านค้า"/>
-        <CardExpand />
+          <SubCard text = "DAISO"/>
+          <SubCard text = "DAISO"/>
+          <SubCard text = "DAISO"/>
+          <SubCard text = "DAISO"/>
       </section>
     );
   };
