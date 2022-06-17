43e8712bcc6cb1334597458ec43c0c6e6c29ab07
OPALKUUUB <makara.atipat@gmail.com>
2022-06-17 14:07:10 +0700

    fix check token

--- client/src/hook/useToken.js
+++ client/src/hook/useToken.js
@@ -3,8 +3,12 @@ import { useState } from "react";
 export default function useToken() {
   const getToken = () => {
     const tokenString = localStorage.getItem("token");
-    const userToken = JSON.parse(tokenString);
-    return userToken?.token;
+    try {
+      const userToken = JSON.parse(tokenString);
+      return userToken?.token;
+    } catch (error) {
+      return false
+    }
   };
   const [token, setToken] = useState(getToken());
   const saveToken = (userToken) => {
